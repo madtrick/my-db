7da34209d14b4bf0a92d3b99b13a94e023c88962
Farruco Sanjurjo <madtrick@gmail.com>
2018-08-02 19:41:39 +0200

    Use all available csv files to compute the results

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 'use strict';
 
 const yargs = require('yargs');
-const inquirer = require('inquirer');
-const Bluebird = require('bluebird');
 const fs = require('fs');
 const JQL = require('yasjql');
 const _ = require('lodash');
 
 const Transactions = require('./lib/transactions');
-
-const co = Bluebird.coroutine;
+const dedupTransactions = require('./lib/dedup-transactions');
 
 const argv = yargs
   .option('source', {
     alias: 's',
     describe: 'Pick a source for the transactions',
-    demandOption: true,
     choices: ['db', 'dkb'],
   }).argv;
 
-function run (t, query) {
+function run (items, query) {
   let bar = query.query;
   if (_.isFunction(bar)) {
     bar = bar();
   }
 
-  let foo = new JQL(t).find(bar.filter);
+  let foo = new JQL(items).find(bar.filter);
 
   if (bar.group) {
     foo = foo.group(bar.group);
@@ -37,11 +33,8 @@ function run (t, query) {
 
   const results = foo.select(bar.select);
 
-  if (query.print) {
-    query.print(results);
-  } else {
-    console.log(results);
-  }
+  console.log(query.title);
+  console.dir(results, { depth: null });
 }
 
 const DBAdapter = require('./lib/adapters/db');
@@ -52,17 +45,65 @@ const ADAPTERS = {
   dkb: DKBAdapter,
 };
 
-co(function* main () {
-  const transactionsSource = argv.source;
-  const csvs = fs.readdirSync(`./transactions/${transactionsSource}`);
-  const files = [
-    { type: 'list', name: 'name', message: 'message', choices: csvs },
+function main () {
+  const transactionsSources = argv.source || ['db', 'dkb'];
+
+  const allTransactions = _.flatten(transactionsSources.map((transactionsSource) => {
+    const csvs = fs.readdirSync(`./transactions/${transactionsSource}`);
+    const csvsData = _.compact(csvs.map((csv) => {
+      const path = `./transactions/${transactionsSource}/${csv}`;
+
+      if (fs.lstatSync(path).isDirectory()) {
+        return null;
+      }
+
+      return fs.readFileSync(path);
+    }));
+    return csvsData.map(data => Transactions.fromCSV(data.toString(), ADAPTERS[transactionsSource]));
+  }));
+
+  const dedupedTransactions = dedupTransactions(allTransactions);
+
+  const beginningMonth = new Date('2018-05-01');
+  const endMonth = new Date('2018-05-31');
+
+  const queries = [
+    {
+      title: 'Salario Contentful',
+      query: {
+        filter: {
+          Date: { gt: beginningMonth, lt: endMonth },
+          Counterparty: { match: /Contentful/ },
+        },
+        group: 'Counterparty',
+        select: { sum: 'Amount' },
+      },
+    },
+    {
+      title: 'Salario Primark',
+      query: {
+        filter: {
+          Amount: { gt: 0 },
+          Date: { gt: beginningMonth, lt: endMonth },
+          Counterparty: { match: /PRIMARK/ },
+        },
+        group: 'Counterparty',
+        select: { sum: 'Amount' },
+      },
+    },
+    {
+      title: 'Gastos',
+      query: {
+        filter: {
+          Amount: { lt: 0 },
+          Date: { gt: beginningMonth, lt: endMonth },
+        },
+        select: { sum: 'Amount' },
+      },
+    },
   ];
 
-  const fileChoice = yield inquirer.prompt(files);
-  const csvData = fs.readFileSync(`./transactions/${transactionsSource}/${fileChoice.name}`);
-  const transactions = Transactions.fromCSV(csvData.toString(), ADAPTERS[transactionsSource]);
+  queries.forEach(query => run(dedupedTransactions.all(), query));
+}
 
-  const query = {}; // placeholder to keep the linter happy
-  run(transactions.items, query);
-})();
+main();
