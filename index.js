e0bf7a1eda805749a617411b453a225f911b5963
Farruco Sanjurjo <madtrick@gmail.com>
2018-09-29 11:36:27 +0200

    Handle files with other encodings than utf8

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const yargs = require('yargs');
 const fs = require('fs');
 const JQL = require('yasjql');
 const _ = require('lodash');
+const iconv = require('iconv-lite');
+const detectCharacterEncoding = require('detect-character-encoding')
 
 const Transactions = require('./lib/transactions');
 const dedupTransactions = require('./lib/dedup-transactions');
@@ -59,15 +61,22 @@ function main () {
         return null;
       }
 
-      return fs.readFileSync(path);
+      if (path.match(/\.DS_Store$/)) {
+        return null;
+      }
+
+      const buffer = fs.readFileSync(path);
+      const detection = detectCharacterEncoding(buffer);
+
+      return iconv.decode(buffer, detection.encoding);
     }));
     return csvsData.map(data => Transactions.fromCSV(data.toString(), ADAPTERS[transactionsSource]));
   }));
 
   const dedupedTransactions = dedupTransactions(allTransactions);
 
-  const beginningMonth = new Date('2018-07-01');
-  const endMonth = new Date('2018-07-31');
+  const beginningMonth = new Date('2018-09-01');
+  const endMonth = new Date('2018-09-30');
 
   const queries = [
     {
@@ -100,7 +109,7 @@ function main () {
           Amount: { gt: 0 },
           Date: { gt: beginningMonth, lt: endMonth },
         },
-        select: { sum: 'Amount' },
+        select: ['Counterparty', 'Amount'],
       },
     },
     {
